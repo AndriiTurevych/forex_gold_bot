d06b90227fc7b0ef763356514e1a893575db5b1c
AndriiTurevych <andrii.turevych@nemiroff.pro>
2026-08-29 15:22:06 +0200

    Export execution safety primitives

--- gold_cio_v9/execution/__init__.py
+++ gold_cio_v9/execution/__init__.py
@@ -1,15 +1,36 @@
 """Deterministic low-latency execution primitives for Gold CIO v9.1."""
 
 from .latency import LatencyBudget, LatencyTrace, StageTiming
+from .latency_stats import LatencyDistribution, summarize_latency
 from .fast_path import FastPathContext, FastPathDecision, FastPathInput, FastPathVerdict, evaluate_fast_path
+from .operational_safety import (
+    IdempotencyRegistry,
+    OperationalDecision,
+    OperationalLimits,
+    OperationalState,
+    deterministic_order_key,
+    evaluate_operational_safety,
+)
+from .order_state import OrderState, OrderStatus, transition
 
 __all__ = [
     "LatencyBudget",
     "LatencyTrace",
     "StageTiming",
+    "LatencyDistribution",
+    "summarize_latency",
     "FastPathContext",
     "FastPathDecision",
     "FastPathInput",
     "FastPathVerdict",
     "evaluate_fast_path",
+    "OperationalLimits",
+    "OperationalState",
+    "OperationalDecision",
+    "evaluate_operational_safety",
+    "deterministic_order_key",
+    "IdempotencyRegistry",
+    "OrderState",
+    "OrderStatus",
+    "transition",
 ]
